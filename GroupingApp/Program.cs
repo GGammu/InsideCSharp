7dea7d0d6f0f9c591e546a60a25a912bbe6e1387
GGammu <ykcul02@gmail.com>
2014-07-17 18:19:51 +0900

    3

---
+++ GroupingApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace GroupingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex r = new Regex("(i(n))g");
+            Match m = r.Match("Matching");
+            GroupCollection gc = m.Groups;
+
+            Console.WriteLine("Found {0} Groups", gc.Count);
+
+            for (int i = 0; i < gc.Count; i++)
+            {
+                Group g = gc[i];
+                Console.WriteLine("Found '{0}' at position {1}", g.Value, g.Index);
+            }
+
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < gc.Count; i++)
+            {
+                CaptureCollection cc = gc[i].Captures;
+
+                for (int j = 0; j < cc.Count; j++)
+                {
+                    Capture cap = cc[j];
+                    Console.WriteLine("Found '{0}' at position {1}", cap.Value, cap.Index);
+                }
+            }
+
+            Console.WriteLine("\n");
+
+            Regex q = new Regex("(?<something>\\w+):(?<another>\\w+)");
+            Match n = q.Match("Salary:123456");
+
+            Console.WriteLine("{0} = {1}", n.Groups["something"].Value, n.Groups["another"].Value);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SplitRegExApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace SplitRegExApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string s = "Once Upon A Time In America";
+
+            //char[] seps = new char[] { ' ' };
+            //foreach (string ss in s.Split(seps))
+            //{
+            //    Console.WriteLine(ss);
+            //}
+
+            //Regex r = new Regex(" ");
+            //foreach (string ss in r.Split(s))
+            //{
+            //    Console.WriteLine(ss);
+            //}
+
+            //string t = "Once,Upon:A/Time\\In\'America";
+
+            //char[] seps = new char[] { ' ', ',', ':', '/', '\\', '\'' };
+            //foreach (string ss in t.Split(seps))
+            //{
+            //    Console.WriteLine(ss);
+            //}
+
+            //Regex r = new Regex(@" |,|/|\\|\'");
+            //foreach (string ss in r.Split(t))
+            //{
+            //    Console.WriteLine(ss);
+            //}
+
+            //string u = "Once Upon A Time In     America";
+
+            //Regex p = new Regex(" ");
+
+            //foreach (string ss in p.Split(u))
+            //{
+            //    if (ss.Length>0)
+            //    {
+            //        Console.WriteLine(ss);
+            //    }
+            //}
+
+            //string v = "Once Upon A Time In         America";
+            //Regex o = new Regex(@"[ ]+");
+            //foreach (string ss in o.Split(v))
+            //{
+            //    Console.WriteLine(ss);
+            //}
+
+            string x = "Once Upon A Time In America";
+            Regex m = new Regex("( )");
+            foreach (string ss in m.Split(x))
+            {
+                Console.WriteLine(ss);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
