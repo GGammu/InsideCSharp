d1a84a10f794b4a54fa1c60deebd66a30920c17e
GGammu <ykcul02@gmail.com>
2014-12-17 18:24:53 +0900

    2014121701

---
+++ GetModulesApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyUtilities;
+using System.Reflection;
+
+class GetModulesApp
+{
+    public static void Main(string[] args)
+    {
+        string assemblyName = AssemblyUtils.GetAssemblyName(args);
+        Console.WriteLine("Loading info for " + assemblyName);
+        Assembly a = Assembly.LoadFrom(assemblyName);
+        Module[] modules = a.GetModules();
+        foreach (Module m in modules)
+        {
+            Console.WriteLine("Module: " + m.Name);
+        }
+    }
+}
---
+++ MyUtilities/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUtilities
+{
+    public class AssemblyUtils
+    {
+        public static string GetAssemblyName(string[] args)
+        {
+            string assemblyName;
+            if (0 == args.Length)
+            {
+                Process p = Process.GetCurrentProcess();
+                assemblyName = p.ProcessName + ".exe";
+            }
+            else
+            {
+                assemblyName = args[0];
+            }
+            return assemblyName;
+        }
+    }
+}
